b1d7559edd3b83877afdac9b88b24c41babff1b6
Cauan Melo <cauandemelo2018@gmail.com>
2025-09-11 04:34:25 +0000

    feat: documentação com swagger de todas as rotas

--- Atividades/ATV-API-simples-com-swagger/app.py
+++ Atividades/ATV-API-simples-com-swagger/app.py
@@ -2,13 +2,72 @@ from flask import Flask, request, jsonify
 from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app, template={
+    "info":{
+        "title": "Minha API Flask",
+        "description": "API feita com Flask e documentação Swagger",
+        "version": "1.0.0",
+    }
+})
 
 users = []
-
 current_id = 0
 
 @app.route('/users', methods=['POST'])
 def create_user():
+    """
+    Cria um novo Usuário
+    ---
+    tags:
+        - Usuários
+    description: Cria um novo usuário com nome e email
+    consume:
+        - application/json
+    produce:
+        - application/json
+    parameters:
+        - in: body
+        name: users
+        description: Objeto json com os dados do Usuário
+        required: True
+        schema:
+            id: UserInput
+            type: object
+            required:
+                - nome
+                - email
+            properties:
+                nome:
+                    type: string
+                    example: João Silva
+                email:
+                    type: string
+                    example: joao@email.com
+    responses:
+        201:
+            description: Usuário criado com sucesso
+            schema:
+                id: UserOutput
+                type: object
+                properties:
+                    id:
+                        type: integer
+                        example: 1
+                    nome:
+                        type: string
+                        example: João Silva
+                    email:
+                        type: string
+                        example: joao@email.com
+        400:
+            description: Requisição Inválida, faltando nome ou email
+            schema:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: "nome e email são obrigatótios"
+    """
     global current_id
     data = request.get_json()
     if not data or 'nome' not in data or 'email' not in data:
@@ -24,10 +83,50 @@ def create_user():
 
 @app.route('/users', methods=['GET'])
 def get_users():
+    """
+    Lista todos os Usuários
+    ---
+    tags:
+        - Usuários
+    description: retorna todos os usuários informando id, nome e email
+    responses:
+        200:
+            description: Lista de usuários retornada com sucesso
+            schema:
+                type: array
+                items:
+                    $ref: '#/definitions/UserOutput' 
+    """
     return jsonify(users), 200
 
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
+    """
+    Busca um usuário específico pelo ID
+    ---
+    tags:
+        - Usuários
+    description: Busca um usuário na lista com base no ID
+    parameters:
+        - name: user_id
+        in: path
+        type: integer
+        required: true
+        description: O ID do usuário a ser buscado
+    responses:
+        200:
+            description: Dados do usuário retornados com sucesso
+            schema:
+                $ref: '#/definitions/UserOutput'
+        404:
+            description: Requisição inválida, usuário não encontrado
+            schema:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: "ID inválido"
+    """
     user = next((user for user in users if user['id'] == user_id), None)
     if user:
         return jsonify(user), 200
@@ -36,6 +135,45 @@ def get_user(user_id):
 
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
+    """
+    Atualiza os dados de um usuário
+    ---
+    tags:
+        - Usuários
+    description: Busca um usuário pelo ID e atualiza seus dados
+    parameters:
+        - name: user_id
+        in: path
+        type: integer
+        required: true
+        description: o ID do usuário a ser atualizado
+        - name: body
+        in: body
+        required: true
+        schema:
+            id: UserUpdateInput
+            type: object
+            properties:
+                nome:
+                    type: string
+                    example: João da Silva
+                email:
+                    type: string
+                    example: joao.silva@email.com
+    responses:
+        200:
+            description: Usuário atualizado com sucesso
+            schema:
+                $ref: '#/definitions/UserOutput'
+        404:
+            description: Requisição inválida, usuário não encontrado
+            schema:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: "ID inválido"
+    """
     user = next((user for user in users if user['id'] == user_id), None)
     if not user:
         return jsonify({'error': 'Usuário não encontrado'}), 404
@@ -46,6 +184,35 @@ def update_user(user_id):
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
+    """
+    Exclui um usuário do sistema
+    ---
+    tags:
+        - Usuários
+    description: Exclui um usuário com base no ID forncido
+    parameters:
+        - name: user_id
+        in: path
+        type: integer
+        required: true
+        description: o ID do usuário a ser excluído
+    responses:
+        200:
+            description: Usuário excluído com sucesso
+            schema:
+                type: object
+                properties:
+                    message:
+                        type: string
+        404:
+            description: Requisição inválida, usuário não encontrado
+            schema:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: "ID inválido"
+    """
     global users
     user = next((user for user in users if user['id'] == user_id), None)
     if not user:
